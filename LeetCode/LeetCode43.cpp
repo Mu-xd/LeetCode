418d32d830078e947de47d7a8fc202404b8ecf34
兴达 穆 <597641037@qq.com>
2020-05-16 11:58:07 +0800

    5/16

--- LeetCode/LeetCode43.cpp
+++ LeetCode/LeetCode43.cpp
@@ -1,78 +1,78 @@
-#include<iostream>
-#include<string>
-using namespace std;
-class Solution {
-public:
-    string multiply(string num1, string num2) {
-		//反转num1 和 num2
-		reverse(num1.begin(), num1.end());
-		reverse(num2.begin(), num2.end());
-		string res = "";
-		for (int i = 0; i < num2.size(); i++)
-		{
-			string rl = "0";
-			for (int j = 0; j < num1.size(); j++)
-			{
-				cout << num1[j] << "*" << num2[i] << endl;
-				string xy = to_string((num1[j] - '0') * (num2[i] - '0'));
-				for (int k = 0; k < j; k++)
-					xy += '0';
-				for (int k = xy.size() - rl.size(); k > 0; k--)
-					rl = '0' + rl;
-				//两数相加 xy 和 rl
-				char flag = '0';
-				for (int k = rl.size() - 1; k >= 0; k--)
-				{
-					int x = (rl[k] + xy[k] + flag - 3 * '0');
-					if (x > 9)
-						flag = x / 10 + '0';
-					else
-						flag = '0';
-					rl[k] = x % 10 + '0';
-				}
-				if (flag != '0')
-					rl = flag + rl;
-			}
-			//rl 是当前计算完的 res是之前计算完的
-			for (int j = 0; j < i; j++)
-			{
-				rl += '0';
-			}
-			for (int j = rl.size() - res.size(); j > 0; j--)
-			{
-				res = '0' + res;
-			}
-			//对rl 和res相加
-			char flag = '0'; 
-			for (int k = rl.size() - 1; k >= 0; k--)
-			{
-				int x = (rl[k] + res[k] + flag - 3 * '0');
-				res[k] = x % 10 + '0';
-				if (x > 9)
-					flag = x /10 + '0';
-				else
-					flag = '0';
-			}
-			if (flag != '0')
-				res = flag + res;
-		}
-		while (res.size() != 0 && res[0] == '0')
-			res = res.substr(1, res.size() - 1);
-		if (res == "")
-			res = "0";
-		return res;
-    }
-};
-int main()
-{
-	while (1)
-	{
-		string num1 = "123456789";
-		string num2 = "9";
-		cin >> num1;
-		cin >> num2;
-		Solution S;
-		cout << num1 << "*" << num2;
-		cout << S.multiply(num1, num2) << endl;;
-	}
-}
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Solution {
+//public:
+//    string multiply(string num1, string num2) {
+//		//反转num1 和 num2
+//		reverse(num1.begin(), num1.end());
+//		reverse(num2.begin(), num2.end());
+//		string res = "";
+//		for (int i = 0; i < num2.size(); i++)
+//		{
+//			string rl = "0";
+//			for (int j = 0; j < num1.size(); j++)
+//			{
+//				cout << num1[j] << "*" << num2[i] << endl;
+//				string xy = to_string((num1[j] - '0') * (num2[i] - '0'));
+//				for (int k = 0; k < j; k++)
+//					xy += '0';
+//				for (int k = xy.size() - rl.size(); k > 0; k--)
+//					rl = '0' + rl;
+//				//两数相加 xy 和 rl
+//				char flag = '0';
+//				for (int k = rl.size() - 1; k >= 0; k--)
+//				{
+//					int x = (rl[k] + xy[k] + flag - 3 * '0');
+//					if (x > 9)
+//						flag = x / 10 + '0';
+//					else
+//						flag = '0';
+//					rl[k] = x % 10 + '0';
+//				}
+//				if (flag != '0')
+//					rl = flag + rl;
+//			}
+//			//rl 是当前计算完的 res是之前计算完的
+//			for (int j = 0; j < i; j++)
+//			{
+//				rl += '0';
+//			}
+//			for (int j = rl.size() - res.size(); j > 0; j--)
+//			{
+//				res = '0' + res;
+//			}
+//			//对rl 和res相加
+//			char flag = '0'; 
+//			for (int k = rl.size() - 1; k >= 0; k--)
+//			{
+//				int x = (rl[k] + res[k] + flag - 3 * '0');
+//				res[k] = x % 10 + '0';
+//				if (x > 9)
+//					flag = x /10 + '0';
+//				else
+//					flag = '0';
+//			}
+//			if (flag != '0')
+//				res = flag + res;
+//		}
+//		while (res.size() != 0 && res[0] == '0')
+//			res = res.substr(1, res.size() - 1);
+//		if (res == "")
+//			res = "0";
+//		return res;
+//    }
+//};
+//int main()
+//{
+//	while (1)
+//	{
+//		string num1 = "123456789";
+//		string num2 = "9";
+//		cin >> num1;
+//		cin >> num2;
+//		Solution S;
+//		cout << num1 << "*" << num2;
+//		cout << S.multiply(num1, num2) << endl;;
+//	}
+//}
---
+++ LeetCode/LeetCode44.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<list>
+#include<map>
+using namespace std;
+class Solution {
+public:
+	bool isMatch(string s, string p) {
+		has = map<list<string>, bool>();
+		return this->isMatchson(s, p);
+	};
+private:
+	map<list<string>, bool> has;
+	bool isMatchson(string s, string p)
+	{
+		int i = 0;
+		for (; i < p.size() - 1; i++)
+		{
+			if (p[i] == '*' && i + 1 < p.size() && p[i] == p[i + 1])
+				;
+			else
+				break;
+		}
+		p = p.substr(i, p.size());
+		if (has.find({ s,p }) != has.end())
+			return has[{s, p}];
+		//结束条件：两个串都为空（匹配成功）两个串中的一个为空（匹配失败） 
+		if ((s == "" && p == "") || p == "*")
+			return true;
+		if (s == "" || p == "")
+			return false;
+		if (p[0] == '*')
+		{
+			auto res = isMatchson(s.substr(1, s.size() - 1), p); 
+			has[{s.substr(1, s.size() - 1), p}] = res;
+			if (res)
+			{
+				return true;
+			}
+			else
+			{
+				auto res1 = isMatchson(s, p.substr(1, p.size() - 1));
+				has[{s, p.substr(1, p.size() - 1)}] = res1;
+				return res1;
+			}
+		}
+		if (p[0] == s[0] || p[0] == '?')
+		{
+			auto res1 = isMatchson(s.substr(1, s.size() - 1), p.substr(1, p.size() - 1));
+			has[{s.substr(1, s.size() - 1), p.substr(1, p.size() - 1)}] = res1;
+			return res1;
+		}
+		else
+			return false;
+	};
+};
+int main()
+{
+	while (true)
+	{
+		string s = "ho";
+		string p = "ho**";
+		cin >> s >> p;
+		Solution S;
+		cout << S.isMatch(s, p)<<endl;
+	}
+}
