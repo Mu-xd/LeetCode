1a5da357e16a31dcb3af830e1434382f3c98fb88
兴达 穆 <597641037@qq.com>
2020-05-29 15:38:32 +0800

    05/29

---
+++ LeetCode/LeetCode198.cpp
@@ -0,0 +1,50 @@
+﻿////----------------------------------------------------------
+////LeetCode198. 打家劫舍
+////你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+////影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+////如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+////给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，
+////一夜之内能够偷窃到的最高金额。
+////-------------------------------------------------------------
+////思路：
+////	动态规划：应该考虑i和 i-1、i-2的关系  
+////	这个问题应该是一个前缀和转换的问题 比较前缀和的大小
+////时间复杂度：  空间复杂度:
+////作者：Mxd
+////时间：2020/05/29
+////-----------------------------------------------------------
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//private:
+//	int recursionRob(vector<int>& nums, int begin) {
+//		//倒序  这里思路有点奇怪
+//		if (nums.size() < begin + 1)
+//			return 0;
+//		else if (nums.size() == begin + 1)
+//			return nums[begin];
+//		int a1 = recursionRob(nums, begin + 2) + nums[begin];//选择了第一个
+//		int a2 = recursionRob(nums, begin + 2 + 1) + nums[begin + 1];//选择了第二个
+//		return a1 > a2 ? a1 : a2;
+//		
+//	}
+//public:
+//	int rob(vector<int>& nums) {
+//		//return recursionRob(nums, 0);
+//		if (nums.size() == 0) return  0;
+//		if (nums.size() == 1) return nums[0];
+//		vector<int> dp = { nums[0] ,nums[0] > nums[1] ? nums[0] : nums[1] };
+//		for (int i = 2; i < nums.size(); i++)
+//		{
+//			dp.push_back(nums[i] > nums[i - 1] ? nums[i] : nums[i - 1]);
+//		}
+//		return dp[dp.size() - 1];
+//	}
+//};
+//
+//int main() {
+//	vector<int> nums = { 2,7,9,1,3,5};
+//	cout << Solution().rob(nums) << endl;
+//}
--- LeetCode/LeetCode25.cpp
+++ LeetCode/LeetCode25.cpp
@@ -1,105 +1,151 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//struct ListNode {
-//    int val;
-//    ListNode *next;
-//    ListNode(int x) : val(x), next(NULL) {}
-//};
-// 
-//class Solution {
-//public:
-//    //简单的用for循环来控制交换
-//    //第一个和第k个交换 第二个和第k-1个交换  依次类推
-//    ListNode* reverseKGroup(ListNode* head, int k) {
-//        //当前指针结点指向头节点的前一个结点
-//        ListNode *p1 = (ListNode*)malloc(sizeof(ListNode));
-//        ListNode* res = p1;
-//        //让p1的下一个结点指向头节点
-//        p1->next = head;
-//        //循环判断当当前指针结点的下一个结点为空时结束循环
-//        while (p1->next != NULL)
-//        {
-//            //进行k/2次循环 依次交换第i个和第k-i个 
-//            int i = 0;
-//            for (; p1->next != NULL && i < k / 2 ;i++)
-//            {
-//                //指向目标结点
-//                ListNode* p2 = p1->next;
-//                //让p2指向目标结点 上一个结点
-//                int j = k - 2*i - 1;
-//                for (; p2->next!= NULL&& j > 1; j--)
-//                {
-//                    p2 = p2->next;
-//                }
-//                //p2指针成功指向了目标结点 //交换结点
-//                if (j == 1 && p2->next != NULL)
-//                {
-//                    //临时的结点用于交换
-//                    ListNode* temp1 = (ListNode*)malloc(sizeof(ListNode));
-//                    ListNode* temp2 = (ListNode*)malloc(sizeof(ListNode));
-//                    temp1 = p1->next;
-//                    temp2 = p2->next;
-//                    //交换相邻的两个和不相邻的分开处理
-//                    if (k - 2 * i == 2)
-//                    {
-//                        p1->next = p2->next;
-//                        temp1->next = temp2->next;
-//                        p1->next->next = temp1;
-//                    }
-//                    else
-//                    {
-//                        p2->next = temp2->next;
-//                        p1->next = temp1->next;
-//                        temp2->next = p1->next;
-//                        p1->next = temp2;
-//                        temp1->next = p2->next;
-//                        p2->next = temp1;
-//                    }
-//                    //让p1指向下一个结点
-//                    p1 = p1->next;
-//                }
-//                //没有找到要交换的结点 说明交换结束
-//                else
-//                {
-//                    return res->next;
-//                }
-//            }
-//            for (;i < k; i++)
-//            {
-//                p1 = p1->next;
-//            }
-//        }
-//        return res->next;
-//    }
-//private:
-//    ListNode* swap(ListNode* head,int i,int j)
-//    {
-//        ;
-//    }
-//};
-//
-//int main()
-//{
-//    vector<int> ss({1,2});
-//    int k = 2;
-//    ListNode* head = (ListNode*)malloc(sizeof(ListNode) * ss.size());
-//    head->next = NULL;
-//    ListNode* p1 = head;
-//    p1->val = ss[0];
-//    for (int i = 1; i < ss.size(); i++)
-//    {
-//        ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
-//        temp->val = ss[i];
-//        temp->next = NULL;
-//        p1->next = temp;
-//        p1 = p1->next;
-//    }
-//    Solution res;
-//    head = res.reverseKGroup(head,k);
-//    while (head!= NULL)
-//    {
-//        std::cout << head->val<<",";
-//        head = head->next;
-//    }
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    //简单的用for循环来控制交换
+    //第一个和第k个交换 第二个和第k-1个交换  依次类推
+    ListNode* reverseKGroup1(ListNode* head, int k) {
+        //当前指针结点指向头节点的前一个结点
+        ListNode *p1 = (ListNode*)malloc(sizeof(ListNode));
+        ListNode* res = p1;
+        //让p1的下一个结点指向头节点
+        p1->next = head;
+        //循环判断当当前指针结点的下一个结点为空时结束循环
+        while (p1->next != NULL)
+        {
+            //进行k/2次循环 依次交换第i个和第k-i个 
+            int i = 0;
+            for (; p1->next != NULL && i < k / 2 ;i++)
+            {
+                //指向目标结点
+                ListNode* p2 = p1->next;
+                //让p2指向目标结点 上一个结点
+                int j = k - 2*i - 1;
+                for (; p2->next!= NULL&& j > 1; j--)
+                {
+                    p2 = p2->next;
+                }
+                //p2指针成功指向了目标结点 //交换结点
+                if (j == 1 && p2->next != NULL)
+                {
+                    //临时的结点用于交换
+                    ListNode* temp1 = (ListNode*)malloc(sizeof(ListNode));
+                    ListNode* temp2 = (ListNode*)malloc(sizeof(ListNode));
+                    temp1 = p1->next;
+                    temp2 = p2->next;
+                    //交换相邻的两个和不相邻的分开处理
+                    if (k - 2 * i == 2)
+                    {
+                        p1->next = p2->next;
+                        temp1->next = temp2->next;
+                        p1->next->next = temp1;
+                    }
+                    else
+                    {
+                        p2->next = temp2->next;
+                        p1->next = temp1->next;
+                        temp2->next = p1->next;
+                        p1->next = temp2;
+                        temp1->next = p2->next;
+                        p2->next = temp1;
+                    }
+                    //让p1指向下一个结点
+                    p1 = p1->next;
+                }
+                //没有找到要交换的结点 说明交换结束
+                else
+                {
+                    return res->next;
+                }
+            }
+            for (;i < k; i++)
+            {
+                p1 = p1->next;
+            }
+        }
+        return res->next;
+    }
+    //交换每一组的第i个和第k-i个-
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* ans = new ListNode(0);
+        ans->next = head;
+        head = ans;
+        while (head->next != nullptr)
+        {
+            int a = k/2; //这在一组中无论是基数还是偶数都只需要交换k/2次
+            for (int i = 0; i < a; i++)
+            {
+                auto e = head;
+                for (int j = 0; j < k - 2 * i - 1; j++)//交换的是第i个和第k-i个-
+                {
+                    e = e->next;
+                    if (e == nullptr)
+                        return ans->next;
+                }
+                if(e->next == nullptr)//要交换的结点时空结点 则返回就可以了
+                    return ans->next;
+                //在这里交换head->next 和 h->next
+                if (head->next == e)//处理相邻结点的问题
+                {
+                    auto temp = e->next->next;
+                    head->next = e->next;
+                    head->next->next = e;
+                    e->next = temp;
+                }
+                else
+                {
+                    auto tempe = e->next, temph = head->next;
+                    auto tempe1 = e->next->next, temph1 = head->next->next;
+                    head->next = tempe;
+                    e->next = temph;
+                    tempe->next = temph1;
+                    temph->next = tempe1;
+                }
+                cout << head->next->val << ":" << e->next->val << endl;
+                head = head->next;
+            }
+            for (int i = 0; i < k - a;i++)
+            {
+                head = head->next;
+            }
+        }
+        return ans->next;
+    }
+private:
+    ListNode* swap(ListNode* head,int k)
+    {
+    }
+};
+
+int main()
+{
+    vector<int> ss({ 1,2,3,4,5 });
+    int k = 3;
+    ListNode* head = (ListNode*)malloc(sizeof(ListNode) * ss.size());
+    head->next = NULL;
+    ListNode* p1 = head;
+    p1->val = ss[0];
+    for (int i = 1; i < ss.size(); i++)
+    {
+        ListNode* temp = (ListNode*)malloc(sizeof(ListNode));
+        temp->val = ss[i];
+        temp->next = NULL;
+        p1->next = temp;
+        p1 = p1->next;
+    }
+    Solution res;
+    head = res.reverseKGroup(head,k);
+    while (head!= NULL)
+    {
+        std::cout << head->val<<",";
+        head = head->next;
+    }
+}
