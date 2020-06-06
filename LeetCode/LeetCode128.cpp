f73812bbf7d28644acf5db8813a7e33a6a9ed9fd
兴达 穆 <597641037@qq.com>
2020-06-06 12:00:18 +0800

    06/06

---
+++ LeetCode/LeetCode128.cpp
@@ -0,0 +1,44 @@
+//----------------------------------------------------------
+//LeetCode128. 最长连续序列
+//给定一个未排序的整数数组，找出最长连续序列的长度。
+//要求算法的时间复杂度为 O(n)。
+//思路：
+//  map(有序性)/并查集（未实现）
+//作者：Mxd
+//时间：2020/06/06
+//-----------------------------------------------------------
+#include<iostream>
+#include<vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        map<int, int> map_res;
+        int lan = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            map_res[nums[i]] = 1;
+        }
+        auto end = --map_res.end();
+        for (auto item = map_res.begin(); item != end;)
+        {
+            int l = item->second;
+            if (item->first + 1 == (++item)->first)
+            {
+                item->second = l + 1;
+                lan = item->second > lan ? item->second : lan;
+            }
+        }
+
+        return lan;
+    }
+};
+int main()
+{
+    vector<int> nums = { 0 };
+    cout << Solution().longestConsecutive(nums) << endl;
+}
--- LeetCode/LeetCodeFace29.cpp
+++ LeetCode/LeetCodeFace29.cpp
@@ -1,60 +1,60 @@
-//----------------------------------------------------------
-//LeetCode面试题29. 顺时针打印矩阵
-//输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
-//----------------------------------------------------------
-//思路：
-//  常规思路
-//作者：Mxd
-//时间：2020/06/05
-//-----------------------------------------------------------
-#include<iostream>
-#include<vector>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if (matrix.size() == 0)
-            return {};
-        vector<int> ans;
-        int min_x = 0, min_y = 0;
-        int max_y = matrix.size() - 1, max_x = matrix[0].size() - 1;
-        int x = 0, y = 0;
-        while (min_x < max_x && min_y < max_y)
-        {
-            //右
-            while (x <= max_x)	ans.push_back(matrix[y][x++]);
-            x = max_x;
-            y++;
-            //下
-            while (y <= max_y)  ans.push_back(matrix[y++][x]);
-            y = max_y;
-            x--;
-            //左
-            while (x >= min_x) ans.push_back(matrix[y][x--]);
-            x = min_x;
-            y--;
-            //上
-            while (y > min_y)  ans.push_back(matrix[y--][x]);
-            max_y--;
-            max_x--;
-            y = ++min_y; x = ++min_x;
-        }
-        if (max_y == min_y && max_x == min_x) ans.push_back(matrix[y][x++]);	//剩一个元素
-        else {
-            if (max_y == min_y)	while (x <= max_x) ans.push_back(matrix[y][x++]);	//剩一行
-            if (max_x == min_x)   while (y <= max_y) ans.push_back(matrix[y++][x]);	//剩一列
-        }
-        return ans;
-    }
-};
-
-int main()
-{
-    vector<vector<int>> matrix = {
-        {1,2,},
-        {5,6,},
-        {9,10},
-    };
-    Solution().spiralOrder(matrix);
-}
+////----------------------------------------------------------
+////LeetCode面试题29. 顺时针打印矩阵
+////输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+////----------------------------------------------------------
+////思路：
+////  常规思路
+////作者：Mxd
+////时间：2020/06/05
+////-----------------------------------------------------------
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+//        if (matrix.size() == 0)
+//            return {};
+//        vector<int> ans;
+//        int min_x = 0, min_y = 0;
+//        int max_y = matrix.size() - 1, max_x = matrix[0].size() - 1;
+//        int x = 0, y = 0;
+//        while (min_x < max_x && min_y < max_y)
+//        {
+//            //右
+//            while (x <= max_x)	ans.push_back(matrix[y][x++]);
+//            x = max_x;
+//            y++;
+//            //下
+//            while (y <= max_y)  ans.push_back(matrix[y++][x]);
+//            y = max_y;
+//            x--;
+//            //左
+//            while (x >= min_x) ans.push_back(matrix[y][x--]);
+//            x = min_x;
+//            y--;
+//            //上
+//            while (y > min_y)  ans.push_back(matrix[y--][x]);
+//            max_y--;
+//            max_x--;
+//            y = ++min_y; x = ++min_x;
+//        }
+//        if (max_y == min_y && max_x == min_x) ans.push_back(matrix[y][x++]);	//剩一个元素
+//        else {
+//            if (max_y == min_y)	while (x <= max_x) ans.push_back(matrix[y][x++]);	//剩一行
+//            if (max_x == min_x)   while (y <= max_y) ans.push_back(matrix[y++][x]);	//剩一列
+//        }
+//        return ans;
+//    }
+//};
+//
+//int main()
+//{
+//    vector<vector<int>> matrix = {
+//        {1,2,},
+//        {5,6,},
+//        {9,10},
+//    };
+//    Solution().spiralOrder(matrix);
+//}
